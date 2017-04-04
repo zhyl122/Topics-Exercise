bb6e42018428e9e719b196cd3b110f5f33f65121
zhouyilin <zhyl9697@gmail.com>
2017-04-05 06:56:43 +0800

    topics app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root "topics#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
 end
